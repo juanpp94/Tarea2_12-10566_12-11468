68a0b6b8eb5b6e3d9962483cfab2ca8637d0436a
vtorres8 <12-11468@usb.ve>
2020-01-31 16:34:48 -0400

    REFACTORING: Metodo 'crearPersona' para disminuir repeticion de crear instancia persona en el codigo

--- Tarea2.py
+++ Tarea2.py
@@ -2,46 +2,46 @@ import unittest
 from Persona import Persona
 class TestPension(unittest.TestCase):
 
+    #SetUp para crear una instancia de persona y no repetir codigo (refactoring)
+    def crearPersona(self):
+        self.p = Persona()
+        self.persona = Persona()
+
     #Caso de Prueba para crear una instancia de la clase Persona
     def test_CrearPersona(self):
-        persona = Persona()
-        self.assertIsInstance(persona,Persona)
+        self.assertIsInstance(self.persona,Persona)
 
     #Caso de Prueba para ver si una persona generica tiene pension
     def test_PensionVejezPersonaGenerica(self):
-        p = Persona()
-        p.tieneDerechoPension()
+        self.p.tieneDerechoPension()
 
     #Caso de Prueba para verificar si un hombre de 70 anos y 750 semanas
     #rotacion tiene pension
     def test_PensionHombre70Anos750SemanasRotacion(self):
-        p = Persona()
-        p.setEdad(70)
-        p.setSemanasRotacion(750)
-        p.tieneDerechoPension()
-        p.setGenero("Hombre")
-        p.tieneDerechoPension()
+        self.p.setEdad(70)
+        self.p.setSemanasRotacion(750)
+        self.p.tieneDerechoPension()
+        self.p.setGenero("Hombre")
+        self.p.tieneDerechoPension()
 
     #Caso de Prueba para verificar si un Hombre de 70 anos y 749 semanas
     #rotacion tiene pension
     def test_PensionHombre70Anos749SemanasRotacion(self):
-        p = Persona()
-        p.setEdad(70)
-        p.setSemanasRotacion(749)
-        p.tieneDerechoPension()
-        p.setGenero("Hombre")
-        p.tieneDerechoPension()
+        self.p.setEdad(70)
+        self.p.setSemanasRotacion(749)
+        self.p.tieneDerechoPension()
+        self.p.setGenero("Hombre")
+        self.p.tieneDerechoPension()
     
     #Hallar la edad minima para que una persona reciba pension
     #Prueba para hombre y mujer
     def test_EdadMinimaParPensionPersonaGenerica(self):
-        p1 = Persona()
-        p2 = Persona()
-        p1.setGenero("Hombre")
-        p2.setGenero("Mujer")
 
-        p1.edadMinimaParaOptarPorPension(p1.genero)
+        self.p.setGenero("Hombre")
+        self.persona.setGenero("Mujer")
 
-        p2.edadMinimaParaOptarPorPension(p2.genero)
+        self.p.edadMinimaParaOptarPorPension(self.p.genero)
+
+        self.persona.edadMinimaParaOptarPorPension(self.persona.genero)
 
 
