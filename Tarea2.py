d097453651e57fd7a34f3ebabaafee10ff102d15
juanpp94 <12-10566@usb.ve>
2020-01-30 21:45:53 -0430

    ERROR: La clase Persona no esta definida

---
+++ Tarea2.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestPension(unittest.TestCase):
+
+    #Caso de Prueba para crear una instancia de la clase Persona
+    def test_CrearPersona(self):
+        persona = Persona()
+        self.assertIsInstance(persona,Persona)
+
